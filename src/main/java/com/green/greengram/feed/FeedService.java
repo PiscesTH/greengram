1be7cc5882ea58882824d45b093961c0cd92dd9f
PiscesTH <kareme007@gmail.com>
2023-11-13 22:18:47 +0900

    23.11.13. home

--- src/main/java/com/green/greengram/feed/FeedService.java
+++ src/main/java/com/green/greengram/feed/FeedService.java
@@ -11,16 +11,17 @@ import java.util.List;
 public class FeedService {
     private final FeedMapper mapper;
 
-    public ResVo insFeed(FeedInsDto dto){
+    public ResVo insFeed(FeedInsDto dto) {
         FeedInsProcDto pDto = new FeedInsProcDto(dto);
         int result = mapper.insFeed(pDto);
 
-        FeedPicsInsProcDto p2Dto = new FeedPicsInsProcDto(pDto.getIfeed(),dto.getPics());
+        FeedPicsInsProcDto p2Dto = new FeedPicsInsProcDto(pDto.getIfeed(), dto.getPics());
         int result2 = mapper.insFeedPic(p2Dto);
         return new ResVo(pDto.getIfeed());
     }
-    public List<FeedSelVo> getFeed(int page){
+
+    public List<FeedSelVo> getFeed(int page) {
         int rowCount = 3;
-        return mapper.selFeed(FeedSelDto.builder().startIdx((page-1)*rowCount).rowCount(rowCount).build());
+        return mapper.selFeed(FeedSelDto.builder().startIdx((page - 1) * rowCount).rowCount(rowCount).build());
     }
 }
--- src/main/java/com/green/greengram/feed/model/FeedSelDto.java
+++ src/main/java/com/green/greengram/feed/model/FeedSelDto.java
@@ -4,8 +4,38 @@ import lombok.Builder;
 import lombok.Getter;
 
 @Getter
-@Builder
 public class FeedSelDto {
     private int startIdx;
     private int rowCount;
+
+    private FeedSelDto(int startIdx, int rowCount) { //private 생성자
+        this.startIdx = startIdx;
+        this.rowCount = rowCount;
+    }
+
+    public static FeedSelDto.builder builder() {
+        return new FeedSelDto.builder();
+    }
+
+    public static class builder {
+        private int startIdx;
+        private int rowCount;
+
+        public builder() {
+        }
+
+        public builder startIdx(int startIdx) {
+            this.startIdx = startIdx;
+            return this;
+        }
+
+        public builder rowCount(int rowCount) {
+            this.rowCount = rowCount;
+            return this;
+        }
+
+        public FeedSelDto build() {
+            return new FeedSelDto(startIdx, rowCount);
+        }
+    }
 }
