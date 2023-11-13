47547c86c25a62ceaef42ae949d46787ad5e874a
PiscesTH <kareme007@gmail.com>
2023-11-13 23:21:46 +0900

    23.11.13. home

--- src/main/java/com/green/greengram/feed/FeedService.java
+++ src/main/java/com/green/greengram/feed/FeedService.java
@@ -14,7 +14,6 @@ public class FeedService {
     public ResVo insFeed(FeedInsDto dto) {
         FeedInsProcDto pDto = new FeedInsProcDto(dto);
         int result = mapper.insFeed(pDto);
-
         FeedPicsInsProcDto p2Dto = new FeedPicsInsProcDto(pDto.getIfeed(), dto.getPics());
         int result2 = mapper.insFeedPic(p2Dto);
         return new ResVo(pDto.getIfeed());
